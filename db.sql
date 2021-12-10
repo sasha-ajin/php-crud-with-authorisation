fab3fe8c97670aa2c59514ad6601726fc52101b3
Sasha Ajintarev <77781886+sasha-ajin@users.noreply.github.com>
2021-12-10 13:55:18 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,103 +1,58 @@
--- phpMyAdmin SQL Dump
--- version 4.9.0.1
--- https://www.phpmyadmin.net/
---
--- Хост: 127.0.0.1:3306
--- Время создания: Апр 23 2020 г., 08:21
--- Версия сервера: 5.7.25
--- Версия PHP: 7.3.9
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
 
 --
--- База данных: `crud`
+-- STRUCTURE TABLE `USERS`
 --
+
+CREATE TABLE PROJECT_1.USERS (
+  idUSERS INT NOT NULL AUTO_INCREMENT,
+  USERNAME VARCHAR(45) NOT NULL,
+  PASSWORD VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`idUSERS`));
+-- --------------------------------------------------------
+
+
+--
+-- STRUCTURE TABLE `TICKETS`
+--
+
+CREATE TABLE PROJECT_1.TICKETS (
+  idTICKETS INT NOT NULL AUTO_INCREMENT,
+  DESCRIPTION VARCHAR(255) NULL,
+  TITLE VARCHAR(45) NULL,
+  PRIMARY KEY (`idTICKETS`));
+-- --------------------------------------------------------
+
+--
+-- DROPING TO TABLE `TICKETS`
+--
+
+INSERT INTO PROJECT_1.TICKETS (`DESCRIPTION`, `TITLE`) VALUES ('SSS', 'NNN');
+INSERT INTO PROJECT_1.TICKETS (`DESCRIPTION`, `TITLE`) VALUES ('DDD', 'DD');
+
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `comments`
+-- DROPING TO TABLE `USERS`
 --
 
-CREATE TABLE `comments` (
-  `id` int(11) NOT NULL,
-  `product_id` int(11) DEFAULT NULL,
-  `body` text COLLATE utf8mb4_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO PROJECT_1.TICKETS (`DESCRIPTION`, `TITLE`) VALUES ('SSS', 'NNN');
+INSERT INTO PROJECT_1.TICKETS (`DESCRIPTION`, `TITLE`) VALUES ('DDD', 'DD');
+
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `products`
+-- DROPING TO TABLE `USERS`
 --
 
-CREATE TABLE `products` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `price` bigint(11) DEFAULT NULL,
-  `description` text COLLATE utf8mb4_unicode_ci
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+INSERT INTO `PROJECT_1`.`USERS` (`USERNAME`, `PASSWORD`) VALUES ('SASHA', 'QWE');
+INSERT INTO `PROJECT_1`.`USERS` (`USERNAME`, `PASSWORD`) VALUES ('KIR', 'QWE');
 
---
--- Дамп данных таблицы `products`
---
-
-INSERT INTO `products` (`id`, `title`, `price`, `description`) VALUES
-(2, 'Product 2', 2000, 'Lorem ipsum 2');
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `comments`
---
-ALTER TABLE `comments`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `product_id` (`product_id`);
-
---
--- Индексы таблицы `products`
---
-ALTER TABLE `products`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `comments`
---
-ALTER TABLE `comments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT для таблицы `products`
---
-ALTER TABLE `products`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `comments`
---
-ALTER TABLE `comments`
-  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `products` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
